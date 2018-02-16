223922398084908035ffd6e4fdd79b00cf9358f7
jcbalunis <jbalunis@mail.csuchico.edu>
2018-02-15 19:38:38 -0800

    full fourune from name

--- main.cpp
+++ main.cpp
@@ -11,18 +11,45 @@ int main()
     int flname;
 
   //get user input
-  cout<< "Please enter you first name. \n ";
+  cout<< "Please enter you first name. \n";
   cin>> fname;
   cout<< "please enter you last name. \n";
   cin>> lname;
   
     
   //tell fortune
-  flname = fname.length() + lname.length();
-  cout<< "Welcome, " <<fname[0]<<lname[0]<< ", here is your fortune...\n";
+  flname = fname.length();
+  cout<< "Welcome, " <<fname[0]<< "." <<lname[0]<< ". , here is your fortune...\n";
   cout<< "Your luck number is "<< flname << ". \n";
 
+  if(fname[0] == 'a' ||
+     fname[0] == 'e' ||
+     fname[0] == 'i' ||
+     fname[0] == 'o' ||
+     fname[0] == 'u' ||
+     fname[0] == 'A' ||
+     fname[0] == 'E' ||
+     fname[0] == 'I' ||
+     fname[0] == 'O' ||
+     fname[0] == 'U')
+    
+    { cout<< "you are destined to be famous! \n";}
+    else
+    { cout<< "you should keep a low profile. \n";} 
 
+  if(  lname[lname.length() - 1] == 'a' ||
+       lname[lname.length() - 1] == 'e' ||
+       lname[lname.length() - 1] == 'i' ||
+       lname[lname.length() - 1] == 'o' ||
+       lname[lname.length() - 1] == 'u' ||
+       lname[lname.length() - 1] == 'A' ||
+       lname[lname.length() - 1] == 'E' ||
+       lname[lname.length() - 1] == 'I' ||
+       lname[lname.length() - 1] == 'O' ||
+       lname[lname.length() - 1] == 'U')
+    { cout<< "you have already met your true love.\n";}
+    
+    cout<< "have a good day!\n";
 
 
   return 0;
